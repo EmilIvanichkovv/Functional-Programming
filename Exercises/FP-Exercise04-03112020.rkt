430a51b174e10f77128be517b7c21438b791bc06
EmilIvanichkov <emil.ivanichkov@gmail.com>
2020-11-30 19:12:29 +0200

    Add Ex.4 notes - Lists

--- Exercises/FP-Exercise04-03112020.rkt
+++ Exercises/FP-Exercise04-03112020.rkt
@@ -1,131 +1,174 @@
+#lang racket
+
+  
 ;УПРАЖНЕНИЕ 4 | СПИСЪЦИ |
 
 ;--------------------------------------------------------
 ;                    Записки
 ;--------------------------------------------------------
 
-; 2 3 5
 
-; cons
-;2   cons
-;   3   cons
-;      5     '()
+;Наредена двойка:
+; (cons 4 "iei")
+; (define p (cons 4 "iei")
+; (car p) -> първи елемент
+; (cdr p) -> втори елемент
+;Наредена тройка:
+; (define q (cons 2 (cons 3 #t)))
+; (car (cdr q)) за достъпване на по вътрешен елемнт
+; (cadr q) - вложени извиквания ( от ляво на дясно)
+; (cddr q) - втори елемнт на втория елемент
+;Атом за празен списък:
+; '()
 
-;cat - глава
-;cdr - опашка
+;Списък - наредена двойка от 1 елемент (глава)
+;и списък с останалите елементи ) (опашка)
 
-;Начини за създаване
-;(cons 2(cons 3(cons 5 '())))
-;'(2 3 5)
-;(list 2 3 5)
-;(list (+ 2 2) 3 5) -> 4 3 5
-;'(2 #f "yey") е валиден списък
+;Пример
+; (cons 2 (cons 3 (cons 5 '())))
+; '(2 3 5) - (за хардкодване)"цитиране" 
+; (list (+ 2 2) 3 5) - за по-сложни списъци
 
 ;Вградени функции:
-;(null? l) проверява празен ли е?
-;(length l) Връща дължината.
-;(reverse l) - обръща го
-;(member x l)- ако х го няма връща лъжа
-;             ако го има връща подсписъка в който се съдържа
-;(map f l) - изпълнява функцията f върху всеки елемент от l
-;(append x y) - слепя два списъка
-;(equal? x y) - сравняване
+; (null? l) - Проверява дали списък е празен
+; (lenght l) - Дължина
+; (reverse l) - Обръща списък
+; (member l) - Проверява дали нещо се съдържа в списък и връща
+;  подсписъкът в който се съдържа
+; (map f l) - Прилага функцията над всеки от елементите и връща
+;  нов списък със приложена функцията
+; (filter p? list) - Прилага предиката над списъка и връща списък
+;  за които той е верен от елементите
+; (append l1 l2) - Слепя два списъка
+; (equal? l1 l2) - Сравнява два списъка
 
 
+;Наши Дефиниции:
 
-;               За Олеснение:
-(define head car);приема списък от неща и връща първото
-(define tail cdr);приема списък от неща и връща списък от вс без първото
+;Lenght:
+ ;(define (length? lst)
+ ;  (if (null? lst) 0
+ ;      (+ 1 (length (cdr lst)))))
+
+ ;(define (legth* lst)
+ ; (define (helper lst res)
+ ;   (if (null? lst)
+ ;       res
+ ;       (helper (tail lst) (+ res 1))))
+ ;(helper lst 0))
+
+;Member:
+ ;(define (member? x lst)
+ ; ; (cond ((null? lst) #f)
+ ; ;       (( = (head lst) x) lst)
+ ; ;       (else (member? x (tail lst)))))
+ ;       или
+ ;  (and (not (null? lst))
+ ;       (or (= (head lst) x)
+ ;           (member? x (tail lst)))))
+
+;Map:
+ ;(define (map? f lst)
+ ;  (if (null? lst) '()
+ ;      (cons (f (head lst))
+ ;            (map? f (tail lst)))))
+
+ ;(define (map* f lst)
+ ; (define (helper lst res)
+ ;   (if (null? lst)
+ ;       res
+ ;       (helper (tail lst)
+                 ;(cons (f (head lst)) res))))  Така новия би бил на обратно
+ ;               (append res (list (f (head lst)))))))
+ ;(helper lst '()))
 
 
+;Filter:
+ ;(define (filter? p? list)
+ ;  (cond ((null lst) '())
+ ;        ((p? (head lst)) (cons (head lst)
+ ;                               (filter p? (tail lst))))
+ ;        (else (filter p? (tail lst)))))
+                         
+        
 
-;;     AKO ТРЯБВА НИЕ ДА ГИ ДЕФИНИРЕАМЕ ТЕ БИХА ИЗГЛЕДАЛ ТАКА:
-
-;(define(length 1st)
- ;  (if(null? lst)0
- ;     (+1 (lenght(crd 1st)))))
-
-;(define(member? x lst)
-  ;cond((null? lst)#f)
-  ;    ((= (head lst) x) #t)
-  ;    ( else(member? x (tail lst)))))
-  ;           или
-  ;(and (not (null? lst))
-  ;      (or (= (head 1st) x)
-  ;           (member? x(tail lst)))))
-
-
-;(define (map f lst)
- ; (if (null? lst) '()
-  ;    (cons (f (head 1st))
-   ;         (map f (tail lst)))))
-
-
-;Функция която филтрира - проверява условие и връща списък с
-;елементите които го изпълняват
-(define (filter p? lst)
-  (cond ((null? lst) '())
-        ((p?(head lst))(cons (head lst)
-                            (filter p? (tail lst))))
-        (else (filter p? (tail lst)))))
-
-;Итеративна версия за ф-я за дължина
-(define (length* lst)
-  (define (helper lst res)
-    (if (null? lst) res
-        (helper (tail lst) (+ res 1))))
-    (helper lst 0))
+;Помощни функции:
+(define head car) ;Приема списък и връща първия елемент
+(define tail cdr) ;Приема списък и връща всичко без първото
 
 ;----------------------------------------------------------------------
 ;                    ЗАДАЧИ
 ;----------------------------------------------------------------------
-;Задача1
+
+
+;Задача 1
 (define (take n lst)
-  (if(or(= n 0)(null? lst))
-     '()
-     (cons (head lst)
-            (take (- n 1) (tail lst)))))
+  (if (or (= n 0) (null? lst))
+      '()
+      (cons (head lst) (take (- n 1) (tail lst)))))
+      
 (define (drop n lst)
   (if (or (= n 0) (null? lst))
       lst
       (drop (- n 1) (tail lst))))
 
+;Задача 2
+(define (all? p? lst)
+  (if (or (null? lst)
+          (and (p? (head lst))
+               (all? p? (tail lst))))
+      #t
+      #f))
+
+(define (any? p? lst)
+  (and (not (null? lst))
+      (or (p? (head lst))
+          (any? p? (tail lst)))))
+
 ;Задача 3
 (define (zip lst1 lst2)
-  (if (or (null? lst1) (null lst2))
+  (if (or (null? lst1) (null? lst2))
       '()
       (cons (cons (head lst1) (head lst2))
-            (zip ((tail lst1) (tail lst2))))))
+            (zip (tail lst1) (tail lst2)))))
+;Задача 4
+(define (zipWith f lst1 lst2)
+  (if (or (null? lst1) (null? lst2))
+      '()
+      (cons (f (head lst1) (head lst2))
+            (zipWith f (tail lst1) (tail lst2)))))
 
-;Задача 4 ???
+;Задачи 3 и 4 от Дренски
+(define (zipWith* f lst1 lst2)
+  (if (or (null? lst1) (null? lst2))
+      '()
+      (cons (f (head lst1) (head lst2))
+            (zipWith* f (tail lst1) (tail lst2)))))
+
+(define (zip* lst1 lst2)
+  (zipWith* cons lst1 lst2))
 
 ;Задача 5
-;note: не може да достъпваме head & tail на пазни списъци!!!
-;за това проверяваме дали списъка не е с 0 или 1 елемнти
-;ако е -> True. В противен случай рекурсивно...
 (define (sorted? lst)
-  (if (or(null? lst)(null? (tail lst)))
-      ;if(< (lenth lst) 2)) Много лоша практика заради рекурсията
-      #t
+  (or (null? lst)
+      (null? (tail lst))
       (and (<= (head lst) (head (tail lst)))
-           (sorted (tail lst))))
+           (sorted? (tail lst)))))
 
-;Ако ни е нужна дължина по време на обработването
-;ни трябва помощтна функция
-(define (smt lst)
-  (define (helper lst n) ; Инварианта (lenth lst = n)
-    (...))
-  (helper lst(lenth lst)))
+; Пример: ако трябва да използваме дължината на
+; подадения списък на всяка итерация.
+   ;(define (neshto lst)
+   ;  (define (helper lst n) ; Инварианта: (length lst) = n
+   ;    (...)) ; Важно - да поддържаме инвариантата при рекурсивните извиквания
+   ;  (helper lst (length lst)))
 
-
-
-
-
-
+          
+          
+;УПРАЖНЕНИЕ 5 | СПИСЪЦИ - ПРОДЪЛЖЕНИЕ
 
 
-
-
+      
+      
 
 
 
@@ -135,4 +178,12 @@
 
 
 
-  
+
+
+
+
+
+
+
+
+
